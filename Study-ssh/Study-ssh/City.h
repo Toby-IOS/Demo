8fb5d63116bfed688b9d49bf20351f765394a038
small T <zhaodi@zhaodidemacbook-air-2.local>
2017-03-12 13:01:56 +0800

    初始化项目并创建City类

---
+++ Study-ssh/Study-ssh/City.h
@@ -0,0 +1,13 @@
+//
+//  City.h
+//  Study-ssh
+//
+//  Created by small T on 17/3/12.
+//  Copyright © 2017年 guojianlong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface City : NSObject
+
+@end
---
+++ Study-ssh/Study-ssh/City.m
@@ -0,0 +1,13 @@
+//
+//  City.m
+//  Study-ssh
+//
+//  Created by small T on 17/3/12.
+//  Copyright © 2017年 guojianlong. All rights reserved.
+//
+
+#import "City.h"
+
+@implementation City
+
+@end
